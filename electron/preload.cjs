cbf776df10c376477545219f05f0f1fa164c0732
Findupto <11663892+findupto@users.noreply.github.com>
2026-09-13 13:36:28 +0500

    Expose printer discovery and print API

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -3,5 +3,7 @@ contextBridge.exposeInMainWorld('posDesktop', {
   getDisplays: () => ipcRenderer.invoke('system:displays'),
   openCustomerDisplay: (id) => ipcRenderer.invoke('display:open', id),
   closeCustomerDisplay: () => ipcRenderer.invoke('display:close'),
-  version: () => ipcRenderer.invoke('app:version')
+  version: () => ipcRenderer.invoke('app:version'),
+  getPrinters: () => ipcRenderer.invoke('printers:list'),
+  print: (payload) => ipcRenderer.invoke('printer:print', payload)
 });
